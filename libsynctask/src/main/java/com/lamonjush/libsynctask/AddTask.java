1d461cc107c80fc5e0497387f8790b115d0347b8
Fahim Salam Chowdhury <fahim.cse11@gmail.com>
2020-05-30 03:36:39 +0600

    refactor

--- libsynctask/src/main/java/com/lamonjush/libsynctask/AddTask.java
+++ libsynctask/src/main/java/com/lamonjush/libsynctask/AddTask.java
@@ -6,20 +6,10 @@ import android.os.Looper;
 import androidx.annotation.NonNull;
 
 import com.lamonjush.libsynctask.callback.TaskEntryListener;
-import com.lamonjush.libsynctask.model.RequestHeader;
 import com.lamonjush.libsynctask.model.Task;
 
-import org.jetbrains.annotations.NotNull;
-
-import java.io.IOException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
 
 public class AddTask {
 
@@ -32,9 +22,7 @@ public class AddTask {
 
             //if network not connected, save the task in the db & update the listener
             if (!SyncTaskLib.getInstance().isNetworkConnected()) {
-                service.submit(() -> {
-                    addTaskToLocalDB(task, listener);
-                });
+                service.submit(() -> addTaskToLocalDB(task, listener));
             }
             //else check internet availability
             else {
@@ -47,9 +35,9 @@ public class AddTask {
                     //if server call success, let user check response, user will notify via `TaskCompleteCallBack` whatever
                     //task is successful or not, if not successful save the task in the db & update the listener.
                     else {
-                        String response = makeServerRequest(task);
+                        String response = Helper.makeServerRequest(task);
                         //server call failed
-                        if (response == null){
+                        if (response == null) {
                             addTaskToLocalDB(task, listener);
                         }
                         //server call successful
@@ -70,65 +58,8 @@ public class AddTask {
         }
     }
 
-    //return null if request failed
-    private String makeServerRequest(@NonNull Task task) {
-        OkHttpClient client = new OkHttpClient();
-        Request request = createRequest(task);
-        try (Response response = client.newCall(request).execute()) {
-            if (response != null && response.body() != null) {
-                return response.body().string();
-            }
-            return null;
-        } catch (IOException e) {
-            return null;
-        }
-    }
-
-    @NotNull
-    private Request createRequest(@NonNull Task task) {
-        Request.Builder requestBuilder = new Request.Builder();
-        //add url
-        requestBuilder = requestBuilder.url(task.getUrl());
-        //method setup with request body
-        switch (task.getInvocationMethod()) {
-            case GET:
-                requestBuilder = requestBuilder.get();
-                break;
-            case DELETE:
-                requestBuilder = requestBuilder.delete();
-                break;
-            case POST:
-                RequestBody body = getRequestBody("");
-                if (task.getRequestBody() != null) {
-                    body = getRequestBody(task.getRequestBody());
-                }
-                requestBuilder = requestBuilder.post(body);
-                break;
-            case PUT:
-                RequestBody body1 = getRequestBody("");
-                if (task.getRequestBody() != null) {
-                    body1 = getRequestBody(task.getRequestBody());
-                }
-                requestBuilder = requestBuilder.put(body1);
-                break;
-        }
-        //add header
-        if (task.getHeaders() != null) {
-            for (RequestHeader header : task.getHeaders()) {
-                requestBuilder = requestBuilder.addHeader(header.getKey(), header.getValue());
-            }
-        }
-        return requestBuilder.build();
-    }
-
-    @NotNull
-    private RequestBody getRequestBody(String body) {
-        MediaType JSON = MediaType.get("application/json; charset=utf-8");
-        return RequestBody.create(body, JSON);
-    }
-
     private void addTaskToLocalDB(@NonNull Task task, TaskEntryListener listener) {
-        saveTaskInDB(task);
+        Helper.saveTaskInDB(task);
         if (listener != null) {
             new Handler(Looper.getMainLooper())
                     .post(listener::onTaskAddedToSyncQueue);
@@ -158,9 +89,5 @@ public class AddTask {
             return false;
         }
         return true;
-    }
-
-    private void saveTaskInDB(@NonNull Task task) {
-        SyncTaskLib.taskDao.insert(task.getTaskEntity());
     }
 }
---
+++ libsynctask/src/main/java/com/lamonjush/libsynctask/Helper.java
@@ -0,0 +1,79 @@
+package com.lamonjush.libsynctask;
+
+import androidx.annotation.NonNull;
+
+import com.lamonjush.libsynctask.model.RequestHeader;
+import com.lamonjush.libsynctask.model.Task;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class Helper {
+    //return null if request failed
+    static String makeServerRequest(@NonNull Task task) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = createRequest(task);
+        try (Response response = client.newCall(request).execute()) {
+            if (response != null && response.body() != null) {
+                return response.body().string();
+            }
+            return null;
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    @NotNull
+    private static Request createRequest(@NonNull Task task) {
+        Request.Builder requestBuilder = new Request.Builder();
+        //add url
+        requestBuilder = requestBuilder.url(task.getUrl());
+        //method setup with request body
+        switch (task.getInvocationMethod()) {
+            case GET:
+                requestBuilder = requestBuilder.get();
+                break;
+            case DELETE:
+                requestBuilder = requestBuilder.delete();
+                break;
+            case POST:
+                RequestBody body = getRequestBody("");
+                if (task.getRequestBody() != null) {
+                    body = getRequestBody(task.getRequestBody());
+                }
+                requestBuilder = requestBuilder.post(body);
+                break;
+            case PUT:
+                RequestBody body1 = getRequestBody("");
+                if (task.getRequestBody() != null) {
+                    body1 = getRequestBody(task.getRequestBody());
+                }
+                requestBuilder = requestBuilder.put(body1);
+                break;
+        }
+        //add header
+        if (task.getHeaders() != null) {
+            for (RequestHeader header : task.getHeaders()) {
+                requestBuilder = requestBuilder.addHeader(header.getKey(), header.getValue());
+            }
+        }
+        return requestBuilder.build();
+    }
+
+    @NotNull
+    private static RequestBody getRequestBody(String body) {
+        MediaType JSON = MediaType.get("application/json; charset=utf-8");
+        return RequestBody.create(body, JSON);
+    }
+
+    static void saveTaskInDB(@NonNull Task task) {
+        SyncTaskLib.taskDao.insert(task.getTaskEntity());
+    }
+}
